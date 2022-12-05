5b3097f194d99d52a86e63795812587e3d43eacf
JhowAndrade <johanny_andrade@yahoo.com.br>
2022-12-05 20:52:02 -0300

    Resposta de validação

--- src/main/java/com/johanny/DSCommerce/dto/ProductDTO.java
+++ src/main/java/com/johanny/DSCommerce/dto/ProductDTO.java
@@ -1,12 +1,22 @@
 package com.johanny.DSCommerce.dto;
 
 import com.johanny.DSCommerce.entities.Product;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 
 public class ProductDTO {
 
     private Long id;
+
+    @Size(min = 3, max = 80, message = "Nome precisa ter de 3 a 80 caracteres")
+    @NotBlank(message = "Campo requerido")
     private String name;
+    @Size(min = 10, message = "Descrição deve ter no mínimo 10 caracteres")
+    @NotBlank(message = "Campo requerido")
     private String description;
+    @Positive(message = "O preço deve ser positivo")
     private Double price;
     private String imgUrl;
 
